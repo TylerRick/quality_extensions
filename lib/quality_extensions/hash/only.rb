97c295a1548d7d226968fe1616b05bd4e7e1cf3c
Tyler Rick <github.com@tylerrick.com>
2009-01-11 14:26:05 -0800

    Original version of Hash#except/#only from http://pastie.org/10707

---
+++ lib/quality_extensions/hash/only.rb
@@ -0,0 +1,19 @@
+# http://pastie.org/10707
+# author unknown, please make yourself known
+# if you would like credit.
+
+class Hash
+  # Usage { :a => 1, :b => 2, :c => 3}.except(:a) -> { :b => 2, :c => 3}
+  def except(*keys)
+    self.reject { |k,v|
+      keys.include? k.to_sym
+    }
+  end
+
+  # Usage { :a => 1, :b => 2, :c => 3}.only(:a) -> {:a => 1}
+  def only(*keys)
+    self.dup.reject { |k,v|
+      !keys.include? k.to_sym
+    }
+  end
+end
