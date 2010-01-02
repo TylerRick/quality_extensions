5f25629e8d4fb1c7ef617e339a6f52f02d3e9166
Tyler Rick <github.com@tylerrick.com>
2010-01-01 16:09:56 -0800

    Also unescape escaped '.' and "\\" characters and any other characters they want to unescape.

--- lib/quality_extensions/regexp/to_plain_s.rb
+++ lib/quality_extensions/regexp/to_plain_s.rb
@@ -8,17 +8,33 @@
 
 
 class Regexp
-  def self.to_plain_s(input)
+  def self.to_plain_s(input, also_unescape = nil)
     input.
     gsub(/((?<!\\)(\\\\)*)\(((?=\?[^):]*:)\?[^):]*:)?/, '\1').   # remove all unescaped '('s and any '?-mix:b-style prefix that their contents begin with
     gsub(/((?<!\\)(\\\\)*)[)?*+\[\]]/, '\1').                    # remove all unescaped ')', '?', '*', etc.es
     gsub(/((?<!\\)(\\\\)*)\{.*\}/, '\1').                        # remove all unescaped /{.*}/
-    gsub(/((?<!\\)(\\\\)*)\\([()])/, '\1\3').                    # unescape any remaining (escaped) '()'s ('\(' and '\)')
+    gsub(/((?<!\\)(\\\\)*)\\([().\/#{also_unescape}])/, '\1\3').                    # unescape any remaining (escaped) '\(', '\)', '\.', etc.es
     gsub(/\\\\/, '\\')
   end
 
-  def to_plain_s
-    Regexp.to_plain_s(self.to_s)
+  # Sometimes you don't want to see a [serialized] version of the regular expression:
+  #   /a/.to_s       => '(?-mix:a)'
+  # and you simply want a simple, human-readable version:
+  #   /a/.to_plain_s => 'a'
+  #
+  # Since the Regexp object doesn't know what it was delimited with, to_plain_s can't know which
+  # delimiter character, if any, it should try to unescape.
+  #
+  # %r#a\#b#.to_plain_s('#') # => "a#b"
+  # %r#a\#b#.to_plain_s      # => "a\\#b"
+  #
+  # An escaped '/' will always be unescaped, since Ruby escapes for you even when you don't:
+  #   /a\/b/.to_plain_s      # => 'a/b'
+  # %r#a\/b#.to_plain_s      # => 'a/b'
+  # %r#a/b#. to_plain_s      # => "a/b"
+  #
+  def to_plain_s(also_unescape = nil)
+    Regexp.to_plain_s(self.to_s, also_unescape)
   end
 end
 
@@ -50,14 +66,14 @@ describe 'Regexp#to_plain_s' do
     /a{1,3}bc/.to_plain_s.should == 'abc'
   end
 
-  it "handles escaped ()'s correctly" do
+  it "unescapes escaped ()'s correctly" do
     #                        1       1 2   1 2
     Regexp.to_plain_s( "(a) \\(b\\) \\\\(c\\\\)" ).should ==
-    'a (b) \c\\'
+    "a (b) \\c\\"
 
     #    1  1  12  12
     /(a) \(b\) \\(c\\)/.to_plain_s.should ==
-    'a (b) \c\\'
+    "a (b) \\c\\"
 
     #                        1   1   1 2   1 2   1 2 3   1 2 3   1 2 3 4   1 2 3 4   1 2 3 4 5   1 2 3 4 5
     Regexp.to_plain_s( "(a) \\(b\\) \\\\(c\\\\) \\\\\\(d\\\\\\) \\\\\\\\(e\\\\\\\\) \\\\\\\\\\(f\\\\\\\\\\)" ).should ==
@@ -68,6 +84,18 @@ describe 'Regexp#to_plain_s' do
     /(a) \(b\) \\(c\\) \\\(d\\\) \\\\(e\\\\) \\\\\(f\\\\\)/.to_plain_s.should ==
     #       1  1  1   1   1 2  1 2  1 2   1 2
     "a (b) \\c\\ \\(d\\) \\\\e\\\\ \\\\(f\\\\)"
+  end
+
+  it "unescapes escaped /'s, iff they tell us that they need to be unescaped" do
+      /a\/b/.to_plain_s.     should == 'a/b'
+    %r#a\#b#.to_plain_s.     should == "a\\#b"
+    %r#a\#b#.to_plain_s('#').should == "a#b"
+    %r#a\/b#.to_plain_s.     should == 'a/b'
+    %r#a/b#. to_plain_s.     should == "a/b"
+  end
+
+  it "unescapes escaped .'s" do
+    /a\.b/.to_plain_s.should == 'a.b'
   end
 
   it 'matches the original regexp' do
